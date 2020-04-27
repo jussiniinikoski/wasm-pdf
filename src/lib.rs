28592c18f22c2707095ba450620ebbc98758cea6
jussiniinikoski <jussi.niinikoski@perjantai.fi>
2020-04-27 12:07:47 +0300

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -19,15 +19,7 @@ extern "C" {
 pub fn run(json: &JsValue) -> Result<(), JsValue> {
     // output panics to console.error
     console_error_panic_hook::set_once();
-    let js_doc: JsDocument = match json.into_serde() {
-        Ok(doc) => doc,
-        Err(e) => {
-            return Err(JsValue::from_str(&format!(
-                "Error. Could not parse JSON data. {}",
-                e
-            )))
-        }
-    };
+    let js_doc = get_js_doc(&json).unwrap();
     let bytes = match pdf::create(&js_doc) {
         Ok(b) => b,
         Err(s) => return Err(s),
@@ -39,16 +31,18 @@ pub fn run(json: &JsValue) -> Result<(), JsValue> {
 /// Test Utility: Exports serde objects to json_out function (JS)
 #[wasm_bindgen]
 pub fn print_document(json: &JsValue) -> Result<(), JsValue> {
-    let js_doc: JsDocument = match json.into_serde() {
-        Ok(doc) => doc,
-        Err(e) => {
-            return Err(JsValue::from_str(&format!(
-                "Error. Could not parse JSON data. {}",
-                e
-            )))
-        }
-    };
+    let js_doc = get_js_doc(&json)?;
     let out = JsValue::from_serde(&js_doc).unwrap();
     json_out(&out);
     Ok(())
+}
+
+fn get_js_doc(json: &JsValue) -> Result<JsDocument, JsValue> {
+    return match json.into_serde() {
+        Ok(doc) => Ok(doc),
+        Err(e) => Err(JsValue::from_str(&format!(
+            "Error. Could not parse JSON data. {}",
+            e
+        ))),
+    };
 }
